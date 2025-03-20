6037f3a7a08100b20f305f5b232d6d59ebbcde71
shishpal <shishpalpolampally@gmail.com>
2025-03-20 12:12:52 +0530

    Implemented Landing Page

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 export const PrivateRoute = ({ children }) => {
-  const isAuthenticated = false; 
+  const isAuthenticated = true; 
 
   return isAuthenticated ? children : <Navigate to="/login" />;
 };
