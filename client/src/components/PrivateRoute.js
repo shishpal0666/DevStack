3fda004d1ae418e262f3edee05d319d07ee12765
shishpal <shishpalpolampally@gmail.com>
2025-03-22 23:29:43 +0530

    Added auth slice

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -2,7 +2,8 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 export const PrivateRoute = ({ children }) => {
-  const isAuthenticated = true; 
+  // const userData = localStorage.getItem("blog-user");
+  const isAuthenticated = true; // Replace with actual authentication check
 
   return isAuthenticated ? children : <Navigate to="/login" />;
 };
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -5,25 +5,30 @@ import LayoutWithoutNavbar from '../components/Layout/WithoutNavbar';
 import { PrivateRoute } from '../components/PrivateRoute';
 import AddBlog from './AddBlog';
 import Blog from './Blog';
+import Bookmarks from './Bookmarks';
 import EditBlog from './EditBlog';
 import LandingPage from './LandingPage';
 import LoginPage from './LoginPage';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import UserPublishedBlogsList from './UserPublishedBlogsList';
 
 const Pages = () => {
   return (
       <Routes>
         <Route element={<LayoutWithNavbar />}>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/bookmarks" element={<PrivateRoute><Bookmarks /></PrivateRoute>} />
+          <Route path="/my-blogs" element={<PrivateRoute><UserPublishedBlogsList /></PrivateRoute>} />
           <Route path="/:id" element={<Blog />} />
         </Route>
         <Route element={<LayoutWithoutNavbar />}> 
           <Route path="/login" element={<LoginPage />} />
           <Route path="/add-blog" element={<PrivateRoute><AddBlog /></PrivateRoute> } />
           <Route path="/edit-blog/:id" element={<PrivateRoute><EditBlog /></PrivateRoute> } />
+          {/* <PrivateRoute path="/add-blog" component={AddBlog} isAuthenticated={isAuthenticated} />
+          <PrivateRoute path="/edit-blog" component={EditBlog} isAuthenticated={isAuthenticated} /> */}
         </Route>
     </Routes>
   )
 }
 
-export default Pages
+export default Pages;
